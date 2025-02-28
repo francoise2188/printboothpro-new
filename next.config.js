cc12870f145eb5e957a957246d78886dee8c57a0
Francoise Toneto <francoise2188@gmail.com>
2025-02-28 17:31:52 -0600

    fix: remove middleware and simplify next config to resolve redirect issues

--- next.config.js
+++ next.config.js
@@ -33,10 +33,6 @@ const nextConfig = {
         source: '/:path*',
         headers: [
           {
-            key: 'Strict-Transport-Security',
-            value: 'max-age=31536000; includeSubDomains'
-          },
-          {
             key: 'Cache-Control',
             value: 'no-store, max-age=0'
           }
